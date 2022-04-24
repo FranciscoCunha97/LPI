1dd8a65550aec87b963bc4af5f9b30a9a6465894
Francisco Cunha <francisco.97@live.com.pt>
2022-04-24 12:14:50 +0100

    tabelas prontas

---
+++ resources/views/allCurriculos.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+
+</head>
+<body>
+
+<!--Navbar-->
+@include('layout.navbarSearchPage');
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container">
+    <h2>This is a Container</h2>
+    <table class="table table-fluid" id="myTable">
+        <thead>
+        <tr><th>Nome</th><th>Tipo</th><th>Titulo</th><th>Data Publicação</th></tr>
+        </thead>
+        <tbody>
+        @foreach($curriculos as $curriculo)
+            <tr><td>{{$curriculo->NomeProfissional}}</td><td>{{$curriculo->Tipo}}</td><td>{{$curriculo->Titulo}}</td><td>{{$curriculo->DataPublicacao}}</td></tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
+<script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+</script>
+</body>
+</html>
